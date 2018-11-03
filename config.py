8d6e4d4451ad9b0d605fd4f6eded6881b89da7da
kumarijyotsna <simmikumari1997@gmail.com>
2018-11-03 16:48:11 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'medicive.db')
